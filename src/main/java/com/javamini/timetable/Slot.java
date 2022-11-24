76b9b02aafed4b6610b9530fcf7a9a23f6da3dba
teja963 <panasateja123@gmail.com>
2022-11-24 13:36:03 +0530

    First Version

---
+++ src/main/java/com/javamini/timetable/Slot.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.javamini.timetable;
+
+public class Slot {
+    public StudentGroup studentgroup;
+    public int teacherid;
+    public String subject;
+
+    Slot() {
+    };
+
+    Slot(StudentGroup studentgroup, int teacherid, String subject) {
+
+        this.studentgroup = studentgroup;
+        this.subject = subject;
+        this.teacherid = teacherid;
+
+    }
+}
